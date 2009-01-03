d44c72c6999922419508d877cf0957d653197403
SND\Cheeso_cp <SND\Cheeso_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2009-01-03 09:18:38 +0000

    git-svn-id: https://dotnetzip.svn.codeplex.com/svn@27373 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ Zlib/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// TODO: Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Revision
+//      Build Number
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.1.1")]
+
+
