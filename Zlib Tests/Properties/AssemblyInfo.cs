5955afba6515e0b9582cbc7a7e36cf8eca68434b
SND\Cheeso_cp <SND\Cheeso_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2009-02-11 08:38:17 +0000

    Update version number to 1.7.2.3
    git-svn-id: https://dotnetzip.svn.codeplex.com/svn@28831 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- Zlib Tests/Properties/AssemblyInfo.cs
+++ Zlib Tests/Properties/AssemblyInfo.cs
@@ -5,14 +5,18 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("ZlibTest")]
 [assembly: AssemblyDescription("test modules for Ionic's ZLIB")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("DotNetZlib Library")]
-[assembly: AssemblyCopyright("Copyright © Dino Chiesa 2009")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.7.2.2")]
-[assembly: AssemblyFileVersion("1.7.2.2")]
+
+
+[assembly: AssemblyVersion("1.7.2.3")]
+[assembly: AssemblyFileVersion("1.7.2.3")]
+
