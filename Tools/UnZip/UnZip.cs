8a07537ae12ace58f95e32ed4948715d74cae667
SND\Cheeso_cp <SND\Cheeso_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2009-11-25 15:14:25 +0000

    myriad minor doc updates
    git-svn-id: https://dotnetzip.svn.codeplex.com/svn@48218 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- Tools/UnZip/UnZip.cs
+++ Tools/UnZip/UnZip.cs
@@ -275,13 +275,13 @@ namespace Ionic.Zip.Examples
                                         if ((zip.Comment != null) && (zip.Comment != ""))
                                             System.Console.WriteLine("Comment: {0}", zip.Comment);
 
-                                        System.Console.WriteLine("\n{1,-22} {2,10}  {3,5}   {4,9}  {5,3} {6,8} {0}",
+                                        System.Console.WriteLine("\n{1,-22} {2,10}  {3,5}   {4,10}  {5,3} {6,8} {0}",
                                                                  "Filename", "Modified", "Size", "Ratio", "Packed", "pw?", "CRC");
                                         System.Console.WriteLine(new System.String('-', 80));
                                         header = false;
                                     }
                                     totalUncompressedSize += e.UncompressedSize;
-                                    System.Console.WriteLine("{1,-22} {2,10} {3,5:F0}%   {4,9}  {5,3} {6:X8} {0}",
+                                    System.Console.WriteLine("{1,-22} {2,10} {3,5:F0}%   {4,10}  {5,3} {6:X8} {0}",
                                                              e.FileName,
                                                              e.LastModified.ToString("yyyy-MM-dd HH:mm:ss"),
                                                              e.UncompressedSize,
@@ -329,7 +329,7 @@ namespace Ionic.Zip.Examples
                             if (!wantQuiet)
                             {
                                 System.Console.WriteLine(new System.String('-', 80));
-                                System.Console.WriteLine("{1,-22} {2,10}  {3,5}   {4,9}  {5,3} {6,8} {0}",
+                                System.Console.WriteLine("{1,-22} {2,10}  {3,5}   {4,10}  {5,3} {6,8} {0}",
                                                          zip.Entries.Count.ToString() + " files", "", totalUncompressedSize, "", "", "", "");
                             }
                         } // else (extract all)
