dfa23b43eeacdce03a54d7b9c1fd678f725a88fd
SND\Cheeso_cp <SND\Cheeso_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2011-07-17 00:27:17 +0000

    workitem 13674 - tweak script to update the version number.
    git-svn-id: https://dotnetzip.svn.codeplex.com/svn@80790 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -1,7 +1,9 @@
 # -------------------------------------------------------
 # SetVersion.ps1
 #
-# Set the version in all the AssemblyInfo.cs or AssemblyInfo.vb files in any subdirectory.
+# Set the version in all the AssemblyInfo.cs or AssemblyInfo.vb files in
+# any subdirectory.  Also set the version in the Product.wxs file, in
+# any subdirectory.
 #
 # usage:
 #  from cmd.exe:
@@ -10,13 +12,14 @@
 #  from powershell.exe prompt:
 #     .\SetVersion.ps1  2.8.3.0
 #
+#
 # This script is part of DotNetZip.
 # DotNetZip is Copyright 2008-2011 Dino Chiesa.
 #
 # DotNetZip is licensed under the MS-PL.  See the accompanying
 # License.txt file.
 #
-# Last Updated: <2011-July-11 18:55:48>
+# Last Updated: <2011-July-16 20:24:36>
 #
 # -------------------------------------------------------
 
@@ -39,7 +42,6 @@ function Update-SourceVersion
 
     $NewVersion = 'AssemblyVersion("' + $Version + '")';
     $NewFileVersion = 'AssemblyFileVersion("' + $Version + '")';
-
 
     foreach ($o in $input)
     {
@@ -67,11 +69,55 @@ function Update-SourceVersion
 }
 
 
+
+
+function Update-SourceWxsVersion
+{
+    Param ([string]$Version)
+
+    $NewVersion = 'productVersion     = "' +  $Version + '"';
+
+    foreach ($o in $input)
+    {
+        $pv = select-string productVersion $o
+
+        if ($pv -ne $null)
+        {
+            Write-output $o.FullName
+            if ($o.Attributes -band [System.IO.FileAttributes]::ReadOnly)
+            {
+                # checkout the file for edit, using the tf.exe too, and
+                # passing the CodePlex authn info on cmd line
+                c:\vs2010\common7\IDE\tf  edit $o.FullName $env:cplogin
+            }
+            $TmpFile = $o.FullName + ".tmp"
+            $newGuid = 'productId          = "'+ [System.Guid]::NewGuid().ToString() + '"'
+
+            get-content $o.FullName |
+                %{$_ -replace 'productVersion *= *"[1-9]+(\.([0-9]+|\*)){1,3}"', $NewVersion } |
+                %{$_ -replace 'productId *= *"([0-9A-Fa-f]){8}-([0-9A-Fa-f]){4}-([0-9A-Fa-f]){4}-([0-9A-Fa-f]){4}-([0-9A-Fa-f]){12}"', $newGuid } > $TmpFile
+
+            move-item $TmpFile $o.FullName -force
+        }
+    }
+}
+
+
+
 function Update-AllAssemblyInfoFiles ( $version )
 {
   foreach ($file in "SolutionInfo.cs", "AssemblyInfo.cs", "AssemblyInfo.vb" )
   {
     get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceVersion $version ;
+  }
+}
+
+
+function Update-AllProductWxsFiles ( $version )
+{
+  foreach ($file in "Product.wxs" )
+  {
+    get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceWxsVersion $version ;
   }
 }
 
@@ -82,6 +128,7 @@ $r= [System.Text.RegularExpressions.Regex]::Match($args[0], "^[0-9]+(\.[0-9]+){1
 if ($r.Success)
 {
   Update-AllAssemblyInfoFiles $args[0];
+  Update-AllProductWxsFiles $args[0];
 }
 else
 {
